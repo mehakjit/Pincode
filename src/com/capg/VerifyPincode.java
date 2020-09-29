98f301486cef8dab5d1102c0773b53ef1d6c4f00
mehakjit <mehakjitsingh@gmail.com>
2020-09-29 08:10:10 +0530

    [Mehakjit] Added: Validation for 6 digit codes

--- src/com/capg/VerifyPincode.java
+++ src/com/capg/VerifyPincode.java
@@ -1,8 +1,22 @@
 package com.capg;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class VerifyPincode {
 	public static void main(String[] args) {
-				System.out.println("Welcome to pincode validation");
-	}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to pincode validation");
+		Pattern pincode = Pattern.compile("[0-9]{6}$");
+		System.out.println("Enter the 6-digit pincode");
+		String pattern = sc.nextLine();
+		Matcher matcher = pincode.matcher(pattern);
+		
+		boolean isFound = matcher.find();
+		if(isFound)
+			System.out.println("Patter found");
+		else
+			System.out.println("Patter not found");
+		}
 }
