327e5c045c87bef4cea11d8f619aefc26db927d7
mehakjit <mehakjitsingh@gmail.com>
2020-09-29 08:14:31 +0530

    [Mehakjit] Added: Restriction for alphabets and special chracter in ending

--- src/com/capg/VerifyPincode.java
+++ src/com/capg/VerifyPincode.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 
 public class VerifyPincode {
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to pincode validation");
 		Pattern pincode = Pattern.compile("^[1-9]{1}[0-9]{5}$");
@@ -18,5 +19,6 @@ public class VerifyPincode {
 			System.out.println("Patter found");
 		else
 			System.out.println("Patter not found");
-		}
+		
+	}
 }
